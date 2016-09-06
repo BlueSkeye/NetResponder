6c5369a2f78fe36189b50ee8c880fbfd5064b5da
BlueSkeye <bsgithub@gmail.com>
2016-09-06 14:45:22 +0200

    Initial seed

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetResponder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetResponder")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bb485682-ced9-442c-9f42-437412a7fb18")]
+[assembly: AssemblyVersion("1.0.*")]
